e9b38da930fac526bbb6046f1ac6e1abe2d453e7
Gnatto <nattapolkaropas@gmail.com>
2020-03-20 18:22:09 -0400

    Add files via upload

---
+++ [WIN10]PrinterDriver&Port.ps1
@@ -0,0 +1,10 @@
+﻿#Simple script requested by colleague to get printer driver and IP address(port) then export it to a CSV file
+Get-Printer |  
+Where-Object PortName -like [1-255]* | 
+Select-Object  Name, DriverName, PortName |
+Export-CSV -Path $home\Desktop\PrinterPorts.csv 
+
+
+
+ 
+
